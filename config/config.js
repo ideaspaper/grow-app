646ba94f35d004e69223d3450ced46baeca0b6d5
ideaspaper <agni.infinity@gmail.com>
2021-02-02 15:19:14 +0700

    heroku deployment

--- config/config.js
+++ config/config.js
@@ -13,9 +13,6 @@ module.exports = {
     "password": process.env.DB_PASS_PROD,
     "database": process.env.DB_DATABASE_PROD,
     "host": process.env.DB_HOST_PROD,
-    "dialect": "postgres",
-    "dialectOptions": {
-      "ssl": true
-    }
+    "dialect": "postgres"
   }
 }
